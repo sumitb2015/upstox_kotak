4b29825f78d269cf161918ccf106982b57263ec9
Sumit Bandyopadhyay <sumitb2015@gmail.com>
2026-03-01 13:54:59 +0530

    chore: cleanup post-development temporary test scripts

--- web_apps/oi_pro/babel_test.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('pcr.html', 'utf8');
-const match = content.match(/<script type="text\/babel">([\s\S]*?)<\/script>/);
-console.log(match ? match[1].substring(0, 100) : "No match");
