b32233ff0e6315e53c78b8e3f2f5cc09b2a6cf4d
Sumit Bandyopadhyay <sumitb2015@gmail.com>
2026-03-04 21:59:44 +0530

    feat: Stock Dashboard Analytics with Buildup quadrant analysis and Redis caching

--- web_apps/oi_pro/sidebar.js
+++ web_apps/oi_pro/sidebar.js
@@ -186,6 +186,11 @@
             svg: '<path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z"></path>'
         },
         {
+            href: "/news-pulse",
+            title: "News Pulse",
+            svg: '<path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2Zm0 0a2 2 0 0 1-2-2v-9c0-1.1.9-2 2-2h2"></path><path d="M18 14h-8"></path><path d="M15 18h-5"></path><path d="M10 6h8v4h-8V6Z"></path>'
+        },
+        {
             href: "/brokers",
             title: "Brokers",
             svg: '<path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><polyline points="16 11 18 13 22 9"></polyline>'
